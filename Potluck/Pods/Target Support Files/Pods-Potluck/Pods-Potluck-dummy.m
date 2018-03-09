58e9755261d7ccc6a46a25d3097405eb9a99cc0d
Jennifer Hamilton <jenniferhamilton@Jennifers-MacBook-Pro.local>
2018-03-09 18:52:44 -0500

    add Firebase SDK

---
+++ Potluck/Pods/Target Support Files/Pods-Potluck/Pods-Potluck-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Potluck : NSObject
+@end
+@implementation PodsDummy_Pods_Potluck
+@end
---
+++ Potluck/Pods/Target Support Files/Pods-PotluckTests/Pods-PotluckTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PotluckTests : NSObject
+@end
+@implementation PodsDummy_Pods_PotluckTests
+@end
