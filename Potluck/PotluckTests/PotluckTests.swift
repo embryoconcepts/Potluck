3268ca6be5b98c7209c704904597311539d672fd
Jennifer Hamilton <jenniferhamilton@Jennifers-MacBook-Pro.local>
2018-03-09 18:45:03 -0500

    initial project creation

---
+++ Potluck/PotluckTests/PotluckTests.swift
@@ -0,0 +1,36 @@
+//
+//  PotluckTests.swift
+//  PotluckTests
+//
+//  Created by Jennifer Hamilton on 3/9/18.
+//  Copyright © 2018 Many Hands Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Potluck
+
+class PotluckTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
