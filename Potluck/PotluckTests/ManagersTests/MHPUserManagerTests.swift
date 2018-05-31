1fd442028ac47a37cf55d79ed0f93153476de31e
Jen Hamilton <embryoconcepts@gmail.com>
2018-05-31 13:09:23 -0400

    add files for initial tests

---
+++ Potluck/PotluckTests/ManagersTests/MHPUserManagerTests.swift
@@ -0,0 +1,46 @@
+//
+//  MHPUserManagerTests.swift
+//  PotluckTests
+//
+//  Created by Jennifer Hamilton on 5/31/18.
+//  Copyright © 2018 Many Hands Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Potluck
+
+class MHPUserManagerTests: XCTestCase {
+    
+    // Class Level - before/after all tests
+    override class func setUp() {
+        super.setUp()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+    }
+    
+    // Instance Level - before/after each test
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    // MARK: - example method
+    func testExampleMethod_shouldReturnSomething() {
+        // Arrange
+        
+        
+        // Act
+        
+        
+        // Assert
+        
+        
+    }
+    
+}
