9fbfdd22b1a6a3f4c7b13ecce9207a89b3efc289
Jen Hamilton <embryoconcepts@gmail.com>
2018-06-25 01:28:08 -0400

    15 - programatically style the text view

--- Potluck/Potluck/ViewControllers/CreateEventControllers/MHPCreateEvent1DetailsViewController.swift
+++ Potluck/Potluck/ViewControllers/CreateEventControllers/MHPCreateEvent1DetailsViewController.swift
@@ -9,21 +9,28 @@
 import UIKit
 
 class MHPCreateEvent1DetailsViewController: UIViewController {
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var datePicker: UIDatePicker!
-   
-    @IBOutlet weak var txtName: UITextField!
-    @IBOutlet weak var txtDescription: UITextView!
-    @IBOutlet weak var txtLocation: UITextField!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtLocation: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
     @IBOutlet weak var btnNext: UIButton!
     
-    
     var mhpUser: MHPUser?
+    let txtViewPlaceholderText = "Describe your event - let guests know if there is a theme, or a special occasion."
+    
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        txtName.delegate = self
+        txtLocation.delegate = self
+        txtDescription.delegate = self
+        
+        setupView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,7 +47,7 @@ class MHPCreateEvent1DetailsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
+
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -48,9 +55,8 @@ class MHPCreateEvent1DetailsViewController: UIViewController {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
      }
-     */
-    
-    
+        
+
     // MARK: - Action Handlers
     
     @IBAction func pickerChanged(_ sender: Any) {
@@ -67,6 +73,15 @@ class MHPCreateEvent1DetailsViewController: UIViewController {
     
     // MARK: - Private methods
     
+    fileprivate func setupView() {
+        txtDescription.text = txtViewPlaceholderText
+        txtDescription.textColor = .lightGray
+        txtDescription.layer.borderColor = UIColor(hexString: "ebebeb").cgColor
+        txtDescription.layer.borderWidth = 1.0
+        txtDescription.layer.cornerRadius = 5
+        
+        datePicker.minimumDate = Date()
+    }
     
     fileprivate func cancel() {
         // TODO: add alert - do you really wanna?
@@ -81,5 +96,36 @@ class MHPCreateEvent1DetailsViewController: UIViewController {
     
     fileprivate func next() {
         // TODO: save current values to user preferences, move to next screen
+    }
+}
+
+extension MHPCreateEvent1DetailsViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = txtViewPlaceholderText
+            textView.textColor = UIColor.lightGray
+        }
+    }
+
+}
+
+extension MHPCreateEvent1DetailsViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case txtName:
+            txtName.becomeFirstResponder()
+        case txtDescription:
+            txtDescription.becomeFirstResponder()
+        default:
+            txtLocation.resignFirstResponder()
+        }
+        return true
     }
 }
